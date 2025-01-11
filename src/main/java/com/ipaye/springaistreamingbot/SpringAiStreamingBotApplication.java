9feba124fcd05d10b4e5d0539b1c0d51660b0b01
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-01-11 11:55:49 +0100

    init commit

---
+++ src/main/java/com/ipaye/springaistreamingbot/SpringAiStreamingBotApplication.java
@@ -0,0 +1,13 @@
+package com.ipaye.springaistreamingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStreamingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiStreamingBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipaye/springaistreamingbot/SpringAiStreamingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipaye.springaistreamingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiStreamingBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
